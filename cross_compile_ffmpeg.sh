4379e742f91c0202873e382bceda131106ca39af
Jan-E <github@ehrhardt.nl>
2014-04-08 18:05:53 +0200

    openssl 1.0.1g

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -781,8 +781,8 @@ build_libaacplus() {
 }
 
 build_openssl() {
-  download_and_unpack_file http://www.openssl.org/source/openssl-1.0.1g.tar.gz openssl
-  cd openssl
+  download_and_unpack_file http://www.openssl.org/source/openssl-1.0.1g.tar.gz openssl-1.0.1g
+  cd openssl-1.0.1g
   export cross="$cross_prefix"
   export CC="${cross}gcc"
   export AR="${cross}ar"
