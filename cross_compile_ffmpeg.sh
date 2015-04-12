502ddc64ae9e3033e8173a38438c585ef56349d5
Jan-E <github@ehrhardt.nl>
2015-04-13 00:40:21 +0200

    FFmpeg 2.6.2

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1252,8 +1252,8 @@ build_ffmpeg() {
 }
 
 build_ffmpeg_release() {
-  local version="2.6.1"
-  local prev_version="2.6"
+  local version="2.6.2"
+  local prev_version="2.6.1"
   local type=$1
   local shared=$2
   local download_url="http://ffmpeg.org/releases/ffmpeg-$version.tar.gz"
