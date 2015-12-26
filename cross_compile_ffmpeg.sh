ca7be4661fca55515960fd92097203e8748cfe2e
Jan-E <github@ehrhardt.nl>
2015-12-26 23:07:27 +0100

    revert --disable-nls in gcc & g++

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -155,7 +155,7 @@ install_cross_compiler() {
   chmod 755 mingw-w64-build-3.6.7.local
   unset CFLAGS # don't want these for the compiler itself since it creates executables to run on the local box
   # pthreads version to avoid having to use cvs for it
-  nice ./mingw-w64-build-3.6.7.local --build-type=multi --clean-build --disable-shared --enable-gendef --default-configure --disable-nls --pthreads-w32-ver=2-9-1 --cpu-count=$gcc_cpu_count --build-type=$build_choice || exit 1 # --disable-shared allows c++ to be distributed at all...which seemed necessary for some random dependency...
+  nice ./mingw-w64-build-3.6.7.local --build-type=multi --clean-build --disable-shared --enable-gendef --default-configure --pthreads-w32-ver=2-9-1 --cpu-count=$gcc_cpu_count --build-type=$build_choice || exit 1 # --disable-shared allows c++ to be distributed at all...which seemed necessary for some random dependency...
   export CFLAGS=$original_cflags # reset it
   if [ -d mingw-w64-x86_64 ]; then
     touch mingw-w64-x86_64/compiler.done
