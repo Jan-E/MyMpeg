99144b6ec6661ec6718203d7b841939c18e82df4
Jan-E <github@ehrhardt.nl>
2015-12-26 13:00:12 +0100

    Fix gnutls install

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -857,7 +857,7 @@ build_gnutls() {
   cd gnutls-$gnutls_version
     sed -i 's/mkstemp(tmpfile)/ -1 /g' src/danetool.c # fix x86_64 absent? but danetool is just an exe AFAICT so this hack should be ok...
     generic_configure "--disable-cxx --disable-doc --enable-local-libopts --disable-guile" # don't need the c++ version, in an effort to cut down on size... XXXX test size difference... libopts to allow building with local autogen installed, guile is so that if it finds guile installed (cygwin did/does) it won't try and link/build to it and fail...
-    do_make_and_make_install
+    do_make_install
   cd ..
   sed -i 's/-lgnutls/-lgnutls -lnettle -lhogweed -lgmp -lcrypt32 -lws2_32 -liconv/' "$PKG_CONFIG_PATH/gnutls.pc"
 }
