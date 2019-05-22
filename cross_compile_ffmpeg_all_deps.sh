e8438f350e34ff140d6311129b02eb683f0c2ced
Jan-E <github@ehrhardt.nl>
2019-05-22 11:25:09 +0200

    Curl 7.65.0

--- cross_compile_ffmpeg_all_deps.sh
+++ cross_compile_ffmpeg_all_deps.sh
@@ -951,8 +951,8 @@ build_nghttp2() {
 }
 
 build_curl() {
-  local curl_version="7.64.1"
-  local curl_previous="7.64.0"
+  local curl_version="7.65.0"
+  local curl_previous="7.64.1"
   rm -rf curl-$curl_previous
   download_and_unpack_file https://curl.haxx.se/download/curl-$curl_version.tar.gz curl-$curl_version
   cd curl-$curl_version
