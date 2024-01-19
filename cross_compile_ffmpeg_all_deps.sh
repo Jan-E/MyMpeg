4dbe092122c32b97d1fe4111f8e301e5883c8352
Jan Ehrhardt <github@ehrhardt.nl>
2024-01-19 15:55:08 +0100

    Curl 8.5.0

--- cross_compile_ffmpeg_all_deps.sh
+++ cross_compile_ffmpeg_all_deps.sh
@@ -1144,8 +1144,8 @@ build_nghttp2() {
 }
 
 build_curl() {
-  local curl_version="8.2.1"
-  local curl_previous="8.2.0"
+  local curl_version="8.5.0"
+  local curl_previous="8.2.1"
   rm -rf curl-$curl_previous
   download_and_unpack_file https://curl.haxx.se/download/curl-$curl_version.tar.gz curl-$curl_version
   cd curl-$curl_version
